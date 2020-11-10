d8ed71f36aaf3d1cbb2f77fdbe1835083cffd854
Prestia East <aitserptia@gmail.com>
2020-11-10 10:21:04 +0800

    first two activities

--- app/src/main/java/com/example/ui/MainActivity.kt
+++ app/src/main/java/com/example/ui/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btn_next).setOnClickListener{nextPage()}
+        val Fname: EditText = findViewById<EditText>(R.id.first_name) as EditText
+        Log.i("firstName", Fname.text.toString())
+        val Lname: EditText = findViewById<EditText>(R.id.last_name) as EditText
+        Log.i("lastName", Lname.text.toString())
+        val Email: EditText = findViewById<EditText>(R.id.email) as EditText
+        Log.i("Email", Email.text.toString())
     }
+
+     private fun nextPage(){
+        val intent = Intent(this, nextPage::class.java)
+        startActivity(intent)
+    }
+
+
 }
--- app/src/main/java/com/example/ui/nextPage.kt
+++ app/src/main/java/com/example/ui/nextPage.kt
@@ -1,11 +1,80 @@
 package com.example.ui
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.Switch
+import android.widget.TextView
+import com.google.android.material.slider.Slider
 
 class nextPage : AppCompatActivity() {
+
+    lateinit var slider: SeekBar
+    lateinit var value: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_next_page)
+
+        slider = findViewById<SeekBar>(R.id.seekBar)
+        value = findViewById<TextView>(R.id.displayAge)
+
+        slider.max = 60
+        slider.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                value.text = "Select Age: " + progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                value.text = " " + seekBar.progress
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                value.text = "Age : " + seekBar.progress
+            }
+        })
+
+
+        val mPickTimeBtn = findViewById<Button>(R.id.birth)
+        val textView = findViewById<TextView>(R.id.birthDate)
+
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+
+        mPickTimeBtn.setOnClickListener {
+
+            val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                // Display Selected date in TextView
+                textView.text = "$dayOfMonth $month, $year"
+            }, year, month, day)
+            dpd.show()
+
+        }
+
+        val ViewGroup = findViewById<Switch>(R.id.switchNum)
+        val numberFormatException = findViewById<TextView>(R.id.number)
+
+        ViewGroup.setOnClickListener{
+            numberFormatException.visibility = if (ViewGroup.visibility == View.INVISIBLE){
+                View.VISIBLE
+            } else {
+                View.VISIBLE
+            }
+        }
+
+
+
+
+
     }
+
+
+
 }
